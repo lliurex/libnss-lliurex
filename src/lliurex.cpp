bd903b3fef8c93ffd941ea531f65507a38d3ff81
Lt-Henry <quiqueiii@gmail.com>
2021-10-14 15:04:15 +0200

    work on getgrent. Untested

--- src/lliurex.cpp
+++ src/lliurex.cpp
@@ -20,6 +20,7 @@
 #include <n4d.hpp>
 
 #include <nss.h>
+#include <systemd/sd-journal.h>
 
 #include <iostream>
 #include <string>
@@ -28,16 +29,34 @@
 using namespace edupals;
 using namespace std;
 
-struct Group
+namespace lliurex
 {
-    std::string name;
-    uint64_t gid;
-    std::vector<std::string> members;
-};
+    struct Group
+    {
+        std::string name;
+        uint64_t gid;
+        std::vector<std::string> members;
+    };
+}
 
 std::mutex mtx;
-std::vector<Group> groups;
+std::vector<lliurex::Group> groups;
 int index = -1;
+
+static int push_string(string in,char** buffer, size_t* remain)
+{
+    size_t fsize = in.size() + 1;
+    if (fsize > *remain) {
+        return -1;
+    }
+    
+    std::memcpy(*buffer,in.c_str(),fsize);
+    
+    *remain -= fsize;
+    *buffer += fize;
+    
+    return 0;
+}
 
 /*
  * Open database
@@ -45,6 +64,7 @@ int index = -1;
 enum nss_status _nss_lliurex_setgrent(int stayopen)
 {
     std::lock_guard<std::mutex> lock(mtx);
+    sd_journal_print(LOG_INFO,"lliurex_setgrent...");
     
     index = -1;
     
@@ -56,7 +76,7 @@ enum nss_status _nss_lliurex_setgrent(int stayopen)
         groups.clear();
         
         for (string key : ret.keys()) {
-            Group grp;
+            lliurex::Group grp;
             
             grp.name = key;
             grp.gid = ret[key][0];
@@ -69,11 +89,12 @@ enum nss_status _nss_lliurex_setgrent(int stayopen)
         }
     }
     catch (std::exception& e) {
+        sd_journal_print(LOG_ERR,"lliurex_setgrent: %s",e.what());
         return NSS_STATUS_UNAVAIL;
     }
     
     index = 0;
-    
+    sd_journal_print(LOG_INFO,"lliurex_setgrent successful");
     return NSS_STATUS_SUCCESS;
 }
 
@@ -82,26 +103,76 @@ enum nss_status _nss_lliurex_setgrent(int stayopen)
 */
 enum nss_status _nss_lliurex_endgrent()
 {
+    sd_journal_print(LOG_INFO,"lliurex_endgrent");
     return NSS_STATUS_SUCCESS;
 }
 
 /*
  * Read group entry
 */
-enum nss_status _nss_lliurex_getgrent_r(struct group *result, char *buffer, size_t buflen, int *errnop)
+enum nss_status _nss_lliurex_getgrent_r(struct group* result, char* buffer, size_t buflen, int* errnop)
 {
+    std::lock_guard<std::mutex> lock(mtx);
+    sd_journal_print(LOG_INFO,"lliurex_getgrent %d",index);
+    
+    if (index == groups.size()) {
+        *errno = NOENT;
+        return NSS_STATUS_NOTFOUND;
+    }
+    
+    char* ptr = buffer;
+    
+    lliurex::Group& grp = groups[index];
+    
+    result->gr_gid = grp.gid;
+    
+    if (push_string(grp.name,&ptr,&buflen) == -1) {
+        *errnop = ERANGE;
+        return NSS_STATUS_TRYAGAIN;
+    }
+    
+    result->gr_name = ptr;
+    vector<char*> tmp;
+    
+    for (string member : grp.members) {
+        if (push_string(member,&ptr,&buflen) == -1) {
+            *errnop = ERANGE;
+            return NSS_STATUS_TRYAGAIN;
+        }
+        tmp.push_back(ptr);
+    }
+    
+    if ( (sizeof(char*)*(tmp.size()+1)) > buflen) {
+        *errnop = ERANGE;
+        return NSS_STATUS_TRYAGAIN;
+    }
+    
+    result->gr_mem = &ptr;
+    
+    int n = 0;
+    
+    for (n = 0;n<tmp.size();n++) {
+        result->gr_mem[n] = tmp[n];
+    }
+    
+    result->gr_mem[n] = 0;
+    
+    index++;
+    return NSS_STATUS_SUCCESS;
 }
 
 /*
  * Find group entry by GID
 */
-enum nss_status _nss_lliurex_getgrgid_r(gid_t gid, struct group *result, char *buffer, size_t buflen, int *errnop)
+enum nss_status _nss_lliurex_getgrgid_r(gid_t gid, struct group* result, char* buffer, size_t buflen, int* errnop)
 {
+    std::lock_guard<std::mutex> lock(mtx);
 }
 
 /*
  * Find group entry by name
 */
-enum nss_status _nss_lliurex_getgrnam_r(const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
+enum nss_status _nss_lliurex_getgrnam_r(const char* name, struct group* result, char *buffer, size_t buflen, int* errnop)
 {
+    std::lock_guard<std::mutex> lock(mtx);
 }
